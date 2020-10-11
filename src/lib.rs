93d8ba3f1ac47a06b6453aa1c854ab870fd9ee67
rival <riccardovalentini1854@gmail.com>
2020-10-11 15:28:29 +0200

    Cleanup Diesel integration: all blocking Diesel operations are now properly handled and offloaded from the server thread. Postgres connections are pooled via R2D2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,56 @@
 #[macro_use]
 extern crate diesel;
-
 pub mod schema;
 pub mod models;
 
 use schema::*;
 use models::*;
 use diesel::prelude::*;
+use diesel::result::Error;
+use r2d2::PooledConnection;
+use diesel::r2d2::ConnectionManager;
 
-pub fn fetch_secrets(connection: &PgConnection) -> Vec<Secret>{
+type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+pub fn fetch_all_secrets(connection: &DbConnection)
+    -> Result<Option<Vec<Secret>>, Error >{
     use schema::secrets::dsl::*;
-    secrets
-        .limit(5)
+    let results = secrets
+        .limit(100)
         .load::<Secret>(connection)
-        .expect("Error loading secrets!")
+        .optional()?;
+    Ok(results)
 }
 
-pub fn create_secret(connection: &PgConnection, secret: &Secret) -> Secret {
-    diesel::insert_into(secrets::table)
+pub fn insert_secret(connection: &DbConnection, secret: &Secret)
+                     -> Result<Secret, Error> {
+    let secret = diesel::insert_into(secrets::table)
         .values(secret)
-        .get_result(connection)
-        .expect("Error saving secret!")
+        .get_result(connection)?;
+    Ok(secret)
+}
+
+pub fn insert_secrets(connection: &DbConnection, secrets: Vec<&Secret>)
+    -> Result<Vec<Secret>, Error > {
+    unimplemented!()
+}
+
+pub fn find_secret_by_id(connection: &DbConnection, id: i32)
+    -> Result<Option<Secret>, Error> {
+    unimplemented!()
+}
+
+pub fn find_secret_by_name(connection: &DbConnection, name: String)
+    -> Result<Option<Secret>, Error>{
+    unimplemented!()
+}
+
+pub fn update_secret_by_id(connection: &DbConnection, id: i32)
+    -> Result<Option<Secret>, Error> {
+    unimplemented!()
+}
+
+pub fn delete_secret_by_id(connection: &DbConnection, id: i32)
+    -> Result<Option<bool>, Error> {
+    unimplemented!()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ extern crate dotenv;
 
 use env_logger::Env;
 use actix_web::middleware::Logger;
-use actix_web::{HttpServer, HttpResponse, Responder, get, put, App, web};
-use diesel::prelude::*;
+use actix_web::{HttpServer, HttpResponse, Responder, get, put, App, web, Error};
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
-use tresor_backend::{fetch_secrets, create_secret};
+use tresor_backend::{fetch_all_secrets, insert_secret};
 use tresor_backend::models::Secret;
+use actix_web::dev::Body;
+use diesel::r2d2::ConnectionManager;
+use r2d2::Pool;
+use actix_web::error::BlockingError;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -19,34 +22,53 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/secrets")]
-async fn get_secrets() -> impl Responder {
+async fn get_secrets(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
     //TODO move connection to application state
-    let connection =  establish_db_connection();
-    let secrets = fetch_secrets(&connection);
-    web::Json(secrets)
+    let connection = data.connection_pool
+        .get()
+        .expect("Could not get DB connection from pool!");
+
+    //web::block() is used to offload the blocking DB operations without blocking the server thread.
+    let maybe_secrets: Option<Vec<Secret>> = web::block(move || fetch_all_secrets(&connection))
+        .await
+        .map_err(handle_internal_server_error)?;
+
+    match maybe_secrets {
+        None => { Ok(HttpResponse::NotFound().body("No secrets found!")) }   //TODO restrict to specific user later
+        Some(secrets) => { Ok(HttpResponse::Ok().json(secrets)) }
+    }
 }
 
 #[put("/secret")]
-async fn put_secret(query: web::Query<Secret>) -> impl Responder {
+async fn put_secret(data: web::Data<AppState>, query: web::Query<Secret>) -> Result<HttpResponse, Error> {
     //TODO do not handle payload via query params
-    //TODO move connection to application state
-    let connection =  establish_db_connection();
-    //TODO implement proper error handling and error response
-    let secret = create_secret(&connection, &query.into_inner());
-    web::Json(secret)
+    let connection = data.connection_pool
+        .get()
+        .expect("Could not get DB connection from pool!");
+
+    //web::block() is used to offload the blocking DB operations without blocking the server thread.
+    let secret: Secret = web::block(move || insert_secret(&connection, &query.into_inner()))
+        .await
+        .map_err(handle_internal_server_error)?;
+
+    Ok(HttpResponse::Ok().json(secret))
 }
 
 //TODO implement DELETE, UPDATE for secret/secrets
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     env_logger::from_env(Env::default().default_filter_or("info")).init();
     info!("Tresor is starting up");
 
-    HttpServer::new(|| {
+    let connection_pool = build_db_connection_pool();
+
+    HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
+            .data(AppState {
+                connection_pool: connection_pool.clone()
+            })
             .service(hello)
             .service(get_secrets)
             .service(put_secret)
@@ -56,11 +78,19 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
-fn establish_db_connection() -> PgConnection {
+struct AppState {
+    connection_pool: Pool<ConnectionManager<PgConnection>>
+}
+
+fn build_db_connection_pool() -> Pool<ConnectionManager<PgConnection>> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL")
         .expect("Database URL for Postgres connection must be set!");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    r2d2::Pool::builder().build(manager).expect("Failed to create DB connection pool!")
+}
 
-    PgConnection::establish(&database_url)
-        .expect(&format!("ERROR connecting to {}", &database_url))
+fn handle_internal_server_error(error: BlockingError<diesel::result::Error>) -> HttpResponse<Body> {
+    error!("{}", error);
+    HttpResponse::InternalServerError().finish()
 }
